f9a71f606a078501f1b809bd2457182f4d79849e
SaiNarsimhaGupta <2023mt93038@wilp.bits-pilani.ac.in>
2024-05-11 14:25:07 +0530

    Added Sample Maven project to test the jenkins job creation

---
+++ Devops_Assignments_2023MT93038/devopsproject/src/main/java/com/bits/assignment/devopsproject/DevopsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bits.assignment.devopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsprojectApplication.class, args);
+	}
+
+}
---
+++ Devops_Assignments_2023MT93038/devopsproject/src/test/java/com/bits/assignment/devopsproject/DevopsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.assignment.devopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
